99deee63832b536a56be1e4e955de1aeb183d3d1
KYHyeon <run190822@gmail.com>
2024-06-29 14:21:13 +0900

    feat: ArticleViewController에 scrollView를 추가해요.

--- TripLogger/Article/ArticleViewController.swift
+++ TripLogger/Article/ArticleViewController.swift
@@ -8,17 +8,28 @@
 import UIKit
 
 import PanModal
+import SnapKit
 
 final class ArticleViewController: UIViewController {
+    
+    private let scrollView = UIScrollView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
 
 extension ArticleViewController: PanModalPresentable {
-    var panScrollable: UIScrollView? { nil }
+    var panScrollable: UIScrollView? { scrollView }
     
     var shortFormHeight: PanModalHeight {
         return .contentHeight(300)
