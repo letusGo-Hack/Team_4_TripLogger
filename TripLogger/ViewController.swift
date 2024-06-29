317d6cc99f0b644e67117b88bf7a3f1b14d37bba
KYHyeon <run190822@gmail.com>
2024-06-29 11:29:37 +0900

    Initial Commit

---
+++ TripLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripLogger
+//
+//  Created by 현기엽 on 6/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
