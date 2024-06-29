876f9c2f8dcddb8aed9dce72d52fed88e29eeebd
mandos1995 <mandos19950620@gmail.com>
2024-06-29 16:13:18 +0900

    feat: 위치정보를 입력받아 현재 날씨 리턴해주는 메서드 추가 및 날씨 조건이 변화할 때, 시간과 바뀐 조건 리턴하는 메서드 구현

--- TripLogger/ViewController.swift
+++ TripLogger/ViewController.swift
@@ -7,30 +7,54 @@
 
 import UIKit
 
+import WeatherKit
+import CoreLocation
+
 import SnapKit
 
 class ViewController: UIViewController {
-
-    // snapkit test
-    private let myView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        return view
-    }()
     
-    private func configureUI() {
-        view.backgroundColor = .yellow
-        view.addSubview(myView)
-        myView.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.size.equalTo(300)
-        }
-    }
+    private var weatherIcon: UIImageView = .init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        configureUI()
+        Task {
+            await configureUI()
+            let a = try await WeatherManager.shared.fetchWeatherInHour(location: .init(
+                latitude: 37.546866198603475,
+                longitude: 127.06629217839286
+            ))
+            
+        }
+    }
+    
+    func configureUI() async {
+        Task {
+            let currentWeatehr = try await WeatherManager.shared.fetchCurrentWeather(
+                location: .init(
+                    latitude: 37.546866198603475,
+                    longitude: 127.06629217839286
+                )
+            )
+            view.backgroundColor = .yellow
+            view.addSubview(weatherIcon)
+            weatherIcon.image = UIImage(systemName: currentWeatehr.symbolName)
+            weatherIcon.snp.makeConstraints {
+                $0.center.equalToSuperview()
+                $0.size.equalTo(300)
+            }
+        }
     }
 }
 
+struct WeatherInfo {
+    var weather: CurrentWeather
+    
+    var image: UIImage? {
+        return UIImage(named: weather.symbolName)
+    }
+    
+    
+    
+}
