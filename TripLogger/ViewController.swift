3ca02ba14971f7f65760564e265fe6f8c9adf776
mandos1995 <mandos19950620@gmail.com>
2024-06-29 12:41:47 +0900

    chore SnapKit SDK install

--- TripLogger/ViewController.swift
+++ TripLogger/ViewController.swift
@@ -7,14 +7,30 @@
 
 import UIKit
 
+import SnapKit
+
 class ViewController: UIViewController {
 
+    // snapkit test
+    private let myView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private func configureUI() {
+        view.backgroundColor = .yellow
+        view.addSubview(myView)
+        myView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.size.equalTo(300)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
+        configureUI()
     }
-
-
 }
 
