18532efde9b5e719c842705886d16302292600fd
KYHyeon <run190822@gmail.com>
2024-06-29 13:41:31 +0900

    feat: 글쓰기 화면을 PanModal을 사용해 띄우도록 수정해요.

--- TripLogger/Article/ArticleViewController.swift
+++ TripLogger/Article/ArticleViewController.swift
@@ -7,9 +7,24 @@
 
 import UIKit
 
+import PanModal
+
 final class ArticleViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+    }
+}
+
+extension ArticleViewController: PanModalPresentable {
+    var panScrollable: UIScrollView? { nil }
+    
+    var shortFormHeight: PanModalHeight {
+        return .contentHeight(300)
+    }
+
+    var longFormHeight: PanModalHeight {
+        return .maxHeightWithTopInset(40)
     }
 }
