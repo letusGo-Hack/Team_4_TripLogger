b1dd43e8fa750fc54fcb48bab4da0278feb744af
KYHyeon <run190822@gmail.com>
2024-06-29 14:21:57 +0900

    feat: Article 컨텐츠뷰에 샘플 화면을 추가해요.
    scrollView를 테스트 하기 위해 사용해요.

--- TripLogger/Article/ArticleViewController.swift
+++ TripLogger/Article/ArticleViewController.swift
@@ -13,11 +13,13 @@ import SnapKit
 final class ArticleViewController: UIViewController {
     
     private let scrollView = UIScrollView()
+    private let stackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureUI()
+        addSampleContents()
     }
     
     private func configureUI() {
@@ -25,6 +27,29 @@ final class ArticleViewController: UIViewController {
         scrollView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        
+        scrollView.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.snp.makeConstraints { make in
+            make.centerX.width.verticalEdges.equalToSuperview()
+        }
+    }
+    
+    private func addSampleContents() {
+        Array(repeating: 0, count: 5)
+            .map { _ in
+                print("!!")
+                return UIColor(red: Double.random(in: (0..<1)), green: Double.random(in: (0..<1)), blue: Double.random(in: (0..<1)), alpha: 1.0)
+            }
+            .map {
+                let square = UIView()
+                square.snp.makeConstraints { make in
+                    make.height.equalTo(200)
+                }
+                square.backgroundColor = $0
+                return square
+            }
+            .forEach { stackView.addArrangedSubview($0) }
     }
 }
 
